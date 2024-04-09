14da86da636d32ea5bfc8dd4eaf2cb23eee9e8b5
Edward Fu <edwardfu850808@gmail.com>
2024-04-09 13:30:58 +0000

    add immich

--- run_containers.sh
+++ run_containers.sh
@@ -4,4 +4,5 @@ docker compose -f docker-compose-media.yml \
   -f docker-compose-applications.yml \
   -f docker-compose-admin.yml \
   -f docker-compose-mealie.yml \
+  -f docker-compose-immich.yml \
   $@
